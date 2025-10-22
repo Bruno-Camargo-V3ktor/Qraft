146b8d900a15c4815e8981d01e670df794e007d9
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-10-22 13:09:18 -0300

    feat: init backend workspace project and defined User model

---
+++ packages/backend/models/src/lib.rs
@@ -0,0 +1,13 @@
+use chrono::{ DateTime, Utc };
+use serde::{ Deserialize, Serialize };
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    pub id: Uuid,
+    pub email: String,
+    pub password_hash: String,
+    pub name: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
